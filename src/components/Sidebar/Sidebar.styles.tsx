267300134ab878c2a5bdc984ee47f11e11a790a6
Falandimho <anthonygames311@gmail.com>
2026-08-25 22:19:29 -0300

    adiciona styled-components e refatora Sidebar para melhorar a navegação

--- src/components/Sidebar/Sidebar.styles.tsx
+++ src/components/Sidebar/Sidebar.styles.tsx
@@ -9,6 +9,7 @@ export const Sidebar = styled.div`
   align-items: center;
   gap: 4px;
   padding: 8px;
+  box-sizing: border-box;
 `;
 
 export const SidebarItemContainer = styled.div`
@@ -19,22 +20,46 @@ export const SidebarItemContainer = styled.div`
   gap: 4px;
   padding: 8px;
   position: relative;
+  box-sizing: border-box;
 `;
 
-export const SidebarItem = styled.a<{ $active: boolean }>`
+// export const SidebarIcon = styled.svg<{ $}`
+
+// `;
+
+export const SidebarLista = styled.ul`
+  width: 100%;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const SidebarItem = styled.li`
+  padding: 8px;
+`;
+
+export const SidebarLink = styled.a<{ $active: boolean }>`
   background-color: ${({ $active }) =>
     $active ? "var(--sidebar-accent)" : "var(--sidebar)"};
   color: var(--sidebar-foreground);
-  border: none ;
+  border: none;
+  border-radius: calc(var(--radius) - 2px);
   font-size: 0.875em;
   font-weight: 400;
   padding: 8px;
   text-align: left;
-  line-height: calc(1.25 - .875);
 
   &:hover,
   :focus {
     background-color: var(--sidebar-accent);
     border: none;
   }
+`;
+
+export const Navegacao = styled.div`
+  color: color-mix(in oklab, var(--sidebar-foreground) 70%, transparent);
+  font-weight: medium;
+  font-size: var(--text-xs);
+  padding: 0px 8px;
 `;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,21 +4,24 @@ import * as Styled from "./Sidebar.styles.tsx";
 export function Sidebar() {
   const [activeItem, setActiveItem] = useState("Relatórios");
   const items = ["Relatórios", "Lançamentos", "Categorias", "Novo lançamento"];
+  // const icons = [""]
 
   return (
     <>
       <Styled.Sidebar>
         <Styled.SidebarItemContainer>
-          <p>Navegação</p>
-          {items.map((item) => (
-            <Styled.SidebarItem
-              key={item}
-              $active={activeItem === item}
-              onClick={() => setActiveItem(item)}
-            >
-              {item}
-            </Styled.SidebarItem>
-          ))}
+          <Styled.Navegacao>Navegação</Styled.Navegacao>
+          <Styled.SidebarLista>
+            {items.map((item) => (
+              <Styled.SidebarLink
+                key={item}
+                $active={activeItem === item}
+                onClick={() => setActiveItem(item)}
+              >
+                {item}
+              </Styled.SidebarLink>
+            ))}
+          </Styled.SidebarLista>
         </Styled.SidebarItemContainer>
       </Styled.Sidebar>
     </>
